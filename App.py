b9ada161596f75bd7004f7b8ea2450ed17aaed37
Asieertxo <asgal.gl@gmail.com>
2022-10-21 02:19:53 +0200

    seccion de faqs hecha

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ from ast import dump
 from flask import Flask, render_template, request, redirect, url_for,flash
 from ensurepip import bootstrap
 from flask_mysqldb import MySQL
+from datetime import date
 import os
 
 app = Flask(__name__)
@@ -13,6 +14,7 @@ app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'restaurante'
 mysql = MySQL(app)
 
+
 # settings
 app.secret_key = "mysecretkey"
 
@@ -23,23 +25,28 @@ app.config['UPLOAD_IMG'] = picFolder
 #importacion de la capeta de imagenes
 styleFolder = os.path.join('static', 'css')
 app.config['UPLOAD_STYLE'] = styleFolder
-css = os.path.join(app.config['UPLOAD_STYLE'], 'style.css')
+css_carta = os.path.join(app.config['UPLOAD_STYLE'], 'style-carta.css')
+css_faqs = os.path.join(app.config['UPLOAD_STYLE'], 'style-faqs.css')
 bootstraps = os.path.join(app.config['UPLOAD_STYLE'], 'bootstrap.min.css')
-styles = [css, bootstraps]
+styles = [bootstraps, css_carta, css_faqs]
 
 
 
+
+
+
+##############################################################################################################PAGINA PRINCIPAL
 @app.route('/')
 def index():
-    conn = mysql.connection.cursor()
-    conn.execute('SELECT * FROM carta')
-    carta = conn.fetchall()
-    return render_template('index.html', carta = carta, styles = styles)
-    return "hola"
+    return render_template('index.html', styles = styles)
 
 
 
 
+
+
+
+##############################################################################################################CARTA
 @app.route('/carta')
 def carta():
     conn = mysql.connection.cursor()
@@ -53,16 +60,80 @@ def carta():
 
 
 
+
+
+
+##############################################################################################################LOCALES
 @app.route('/locales')
 def locales():
     return "locales"
     return render_template('locales.html', styles = styles)
 
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################################################FAQS
 @app.route('/faqs')
 def faqs():
-    return "preguntas"
-    return render_template('faqs.html', styles = styles)
 
+
+
+    conn = mysql.connection.cursor()
+    conn.execute('SELECT * FROM faqs')
+    faqs = conn.fetchall()
+    return render_template('faqs.html',faqs = faqs, styles = styles)
+
+
+@app.route('/añadir_comentario', methods=['POST'])
+def añadir_comentario():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        email = request.form['email']
+        mensaje = request.form['mensaje']
+        likes = 0
+        fecha = date.today()
+        print(fecha)
+
+        conn = mysql.connection.cursor()
+        conn.execute('INSERT INTO faqs(nombre, apellido, email, mensaje, likes, fecha) VALUES (%s, %s, %s, %s, %s, %s)',
+        (nombre, apellido, email, mensaje, likes, fecha))
+        flash('Pregunta guardada')
+        mysql.connection.commit()
+
+        return redirect(url_for('faqs'))
+
+
+@app.route('/sumar_like', methods=['POST'])
+def sumar_like():
+    if request.method == 'POST':
+        id = request.form['id']
+        likes = int(request.form['likes']) + 1
+
+        conn = mysql.connection.cursor()
+        conn.execute('UPDATE faqs SET likes = %s WHERE id = %s',
+        (likes, id))
+        mysql.connection.commit()
+
+        return redirect(url_for('faqs'))
+
+
+
+
+
+
+
+
+##############################################################################################################PEDIDOS
 @app.route('/pedido')
 def pedido():
     return "pedido"
